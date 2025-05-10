c522df310818a5c149a2859472f8593f653579b8
gmhridu <hasanhridoymahabub9@gmail.com>
2025-05-10 08:20:02 +0600

    changes made in axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://oustkotek-backend.vercel.app/api/v1', 
+  baseURL: 'https://oustkotek-backend.onrender.com/api/v1', 
   withCredentials: true,
 });
 
